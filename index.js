6b4005f0089e957638daea99e1dfbd903e64c2a5
Jesús Antonio García Valadez <jesus.garciav@me.com>
2020-11-24 20:21:35 -0600

    feat: added prices, cursor and score legend

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ const WALL_WIDTH = '5'
 const WALL_DEPTH = '5'
 const WALLS_CONTAINER = document.querySelector('#wallsContainer')
 const PRICES_CONTAINER = document.querySelector('#pricesContainer')
+const SCORE_CONTAINER = document.querySelector('#score')
 
-const gameMap = [
+const GAME_MAP = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
@@ -37,16 +38,16 @@ const createWall = position => {
 
 const createPrice = position => {
     const price = document.createElement('a-sphere')
-    WALLS_CONTAINER.appendChild(price)
+    PRICES_CONTAINER.appendChild(price)
     price.setAttribute('position', position)
     price.setAttribute('class', 'price')
     price.setAttribute('color', 'orange')
     price.setAttribute('radius', 1)
 }
 
-gameMap.map((set, setIndex) => {
+GAME_MAP.map((set, setIndex) => {
     set.map((element, elementIndex) => {
-        const position = (setIndex - gameMap.length / 2) * WALL_WIDTH + ' ' + 1.5 + ' ' + (elementIndex - set.length / 2) * WALL_WIDTH
+        const position = (setIndex - GAME_MAP.length / 2) * WALL_WIDTH + ' ' + 1.5 + ' ' + (elementIndex - set.length / 2) * WALL_WIDTH
 
         switch(element) {
             case WALL:
@@ -59,6 +60,23 @@ gameMap.map((set, setIndex) => {
             case PRICE:
                 createPrice(position)
                 break
+        }
+    })
+})
+
+const prices = Array.from(document.querySelectorAll('.prices'))
+const score = prices.length
+
+SCORE_CONTAINER.setAttribute('value', `You have ${score} prices left to win!`)
+
+prices.map(price => {
+    price.addEventListener('click', function () {
+        price.setAttribute('visible', 'false')
+        score--
+        SCORE_CONTAINER.setAttribute('value', `You have ${score} prices left to win!`)
+
+        if (score <= 0) {
+            SCORE_CONTAINER.setAttribute('value', `You won!`)
         }
     })
 })
